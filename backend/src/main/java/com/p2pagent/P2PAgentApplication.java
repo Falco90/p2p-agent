ffd516f9f0e7d42efb8b62e29dea633ef1f86fc5
Falco90 <falcorodenburg@gmail.com>
2026-05-03 16:49:54 +0700

    add @ConfigurationPropertiesScan annotation to main

--- backend/src/main/java/com/p2pagent/P2PAgentApplication.java
+++ backend/src/main/java/com/p2pagent/P2PAgentApplication.java
@@ -2,8 +2,10 @@ package com.p2pagent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class P2PAgentApplication {
 
 	public static void main(String[] args) {
